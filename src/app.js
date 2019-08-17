7d7e7c60ace97c1ba6f48fb5b505fd6fb19c6fde
Your Name <you@example.com>
2019-08-17 17:05:23 -0600

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const forecast = require('./utils/forecast')
 const geocode = require('./utils/geocode')
 
 const app = express()
+const port=process.env.PORT || 3000
 
 //Define paths for express config
 const directoryPath = path.join(__dirname, '../public')
@@ -105,8 +106,8 @@ app.get('*', (req, res) => {
 
     })
 })
-app.listen('3000', () => {
-    console.log('server started on port 3000')
+app.listen(port, () => {
+    console.log('server started on port '+port)
 })
 
 
